acdf2003b1b8aabdd8c39b9af1fc4d777b241833
Uriel Arevalo <urievalo@hotmail.com>
2020-08-20 15:46:35 -0500

    Uralberto-Proyect kick off

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+// importa la librería que se bajó con npm.
+// Es una libería porque no tiene punto o barra o no tiene un path relativo
+// Se está haciendo referencia a una dependencia de npm
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import router from '@/router'
+import { dollarFilter } from '@/filters'
+import { percentFilter } from '@/filters'
+import Chart from 'chart.js'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+
+// el @ es un pequeño truco que nos da webpack y vue para hacer referencia de manera relativa al directorio src. Es decir, @ representa el directorio SRC. Lo bueno de esto es que si vamos a utilizar este archivo en otro archivo no ncesitamos carmbiar la rura porque no es relativa.
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+
+Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
+
+// La función .$mount("#app") tiene el mismo propósito que el elemento <el>
+// la función render es una forma de reemplazar la instancia  o el seteo de la aplicación dentro de nuestro componente
